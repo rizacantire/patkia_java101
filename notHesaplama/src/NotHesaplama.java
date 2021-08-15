236b9309a3d6aad5bf166e9c01c16fb7b7aac60d
rza35 <riza_tire@hotmail.com>
2021-08-16 00:18:53 +0300

    Not hesaplama uygulaması

--- notHesaplama/src/NotHesaplama.java
+++ notHesaplama/src/NotHesaplama.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class NotHesaplama {
     public static void main(String[] args) {
-        
+        int mat,fizik,kimya,tarih,muzik,ortalama,toplam;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Matematik notunuzu girin : ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunuzu girin : ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunuzu girin : ");
+        kimya = input.nextInt();
+
+        System.out.print("Tarih notunuzu girin : ");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik notunuzu girin : ");
+        muzik = input.nextInt();
+
+        toplam = fizik + mat + tarih + kimya + muzik;
+        ortalama = toplam / 5 ;
+        System.out.println("Ortalamanız: " + ortalama );
+
+        boolean sonuc = ortalama >= 60;
+        System.out.println(sonuc ? "Geçti" : "Kaldı");
     }
 }
