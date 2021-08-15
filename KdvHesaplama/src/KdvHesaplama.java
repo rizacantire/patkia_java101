ba846e905736ff353c815adc6a8f6cce0ee1fc87
rza35 <riza_tire@hotmail.com>
2021-08-16 00:18:53 +0300

    Not hesaplama uygulaması

---
+++ KdvHesaplama/src/KdvHesaplama.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        double fiyat,kdv1,kdv2,fiyatKdv;
+        kdv1 = 0.18;
+        kdv2 = 0.08;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fiyat giriniz : ");
+        fiyat = input.nextDouble();
+
+        boolean say = fiyat >1000;
+        fiyatKdv = say ? (fiyat += fiyat * kdv1) : (fiyat += fiyat * kdv2);
+
+        System.out.println("fiyat : " +fiyatKdv);
+    }
+}
