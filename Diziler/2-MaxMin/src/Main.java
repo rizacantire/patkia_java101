33fa391a77e3f13860bf8038bbbab5239c900ef5
rza35 <riza_tire@hotmail.com>
2021-08-22 22:24:46 +0300

    Max

--- Diziler/2-MaxMin/src/Main.java
+++ Diziler/2-MaxMin/src/Main.java
@@ -1,21 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] list = {56, 34, 1, 8, 101, -2, -33};
+        int[] list = {15,12,788,1,-1,-778,2,0};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        int sayi = scanner.nextInt();
 
-        int min = list[0];
-        int max = list[0];
+        int min = 0;
+        int max = 1000;
 
         for (int i : list) {
-            if (i < min) {
-                min = i;
+            if (i < sayi) {
+                if(i > min){
+                    min = i;
+                }
             }
-            if (i > max) {
-                max = i;
+            if (i > sayi) {
+                if(i < max){
+                    max = i;
+                }
             }
         }
 
-        System.out.println("Minimum Değer " + min);
-        System.out.println("Maximum Değer " + max);
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + min);
+        System.out.println("Girilen sayıdana büyük en yakın sayı :  " + max);
 
     }
 }
